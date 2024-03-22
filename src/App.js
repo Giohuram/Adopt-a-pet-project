b9668a2cb502e8f14129158407bbe81615a99592
Giovanni Masala <giovannimasala@Giovannis-MacBook-Pro.local>
2024-03-22 04:49:42 +0100

    refactored indexjs from detail component to display animals single page with their details

--- src/App.js
+++ src/App.js
@@ -8,9 +8,14 @@ import Root from './components/root';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 // create router with JSX Route elements
-const appRouter = createRoutesFromElements(
-<Route path='/' element={<Root/>} />
-);
+const appRouter = createBrowserRouter(createRoutesFromElements(
+<Route path='/' element={<Root/>}>
+  <Route index element={ <HomePage/> }/>
+  <Route path=':type' element={<HomePage/>} />
+  <Route path=":type/:id" element={ <PetDetailsPage/> } />
+</Route>
+
+));
 
 function App() {
   return (
